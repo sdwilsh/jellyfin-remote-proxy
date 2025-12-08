46e53ff121f2c66d166979fed8b799cfc75796e5
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2025-12-07 23:07:11 -0800

    refactor: use simpler proxy setup with tokio

--- src/discovery.rs
+++ src/discovery.rs
@@ -2,9 +2,8 @@ use std::net::IpAddr;
 use std::net::Ipv4Addr;
 use std::net::SocketAddr;
 use std::net::SocketAddrV4;
-use std::net::UdpSocket;
 use std::str;
-use std::thread::{self, JoinHandle};
+use tokio::net::UdpSocket;
 
 use log::{debug, info};
 use serde::{Deserialize, Serialize};
@@ -36,44 +35,53 @@ impl DiscoveryResponse {
     }
 }
 
-pub fn query_proxied_server(remote_address: &IpAddr) -> DiscoveryResponse {
+pub async fn query_proxied_server(
+    remote_address: &IpAddr,
+) -> Result<DiscoveryResponse, Box<dyn std::error::Error>> {
     let discovery_addr = SocketAddr::new(*remote_address, DISCOVERY_PORT);
     info!("Querying {:?} for Jellyfin information...", discovery_addr);
     let discovery_socket =
         UdpSocket::bind(SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), ANY_PORT))
+            .await
             .expect("Could not create UDP socket!");
     discovery_socket
         .send_to(DISCOVERY_QUERY.as_bytes(), discovery_addr)
+        .await
         .expect("Unable to connect to remote Jellyfin server!");
 
     let mut buf = [0; 1024];
     let bytes = discovery_socket
         .recv(&mut buf)
+        .await
         .expect("Failed to receive data from remote Jellyfin server!");
     debug!("Received {:?} bytes from {:?}", bytes, discovery_addr);
 
     let deserialized: DiscoveryResponse =
         serde_json::from_slice(&buf[0..bytes]).expect("Unable to deserialize server response!");
-    return deserialized;
+    Ok(deserialized)
 }
 
-pub fn start(
+pub async fn start(
     discovery_response: DiscoveryResponse,
     local_socket_address: &SocketAddrV4,
-) -> JoinHandle<()> {
+) -> Result<
+    tokio::task::JoinHandle<Result<(), Box<dyn std::error::Error + Send>>>,
+    Box<dyn std::error::Error>,
+> {
     let response = serde_json::to_string(&DiscoveryResponse {
         address: format!("http://{}", local_socket_address.to_string().as_str()),
         ..discovery_response
     })
     .expect("Unable to serialize local discovery response!");
 
-    let worker_thread_join_handle = thread::spawn(move || {
-        let discovery_addr: SocketAddr = (IpAddr::V4(Ipv4Addr::UNSPECIFIED), DISCOVERY_PORT).into();
-        let socket =
-            UdpSocket::bind(discovery_addr).expect("Could not bind to address for auto-discovery!");
+    let discovery_addr: SocketAddr = (IpAddr::V4(Ipv4Addr::UNSPECIFIED), DISCOVERY_PORT).into();
+    let socket = UdpSocket::bind(discovery_addr)
+        .await
+        .expect("Could not bind to address for auto-discovery!");
 
+    Ok(tokio::spawn(async move {
         let mut buf = [0; DISCOVERY_QUERY.as_bytes().len()];
-        while let Ok((_, address)) = socket.recv_from(&mut buf) {
+        while let Ok((_, address)) = socket.recv_from(&mut buf).await {
             debug!("Received potential discovery request from {:?}", address);
             let message = match str::from_utf8(&buf) {
                 Ok(v) => v,
@@ -92,10 +100,10 @@ pub fn start(
             info!("Responding to {:?} for a discovery query.", address);
             socket
                 .send_to(response.as_bytes(), address)
+                .await
                 .expect("Unable to connect to respond to query!");
         }
-        debug!("Done receivng from socket.")
-    });
-
-    return worker_thread_join_handle;
+        debug!("Done receivng from socket.");
+        Ok(())
+    }))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-extern crate anyhow;
 extern crate env_logger;
 extern crate log;
-extern crate time;
+extern crate tokio;
 use discovery::query_proxied_server;
 
 use std::env;
@@ -11,16 +10,23 @@ mod config;
 mod discovery;
 mod proxy;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     let args: Vec<String> = env::args().collect();
-    let config = config::load(&args);
+    let config = config::load(&args).await.expect("Unable to load config!");
 
     let remote_address = config.remote.as_ip_addr();
-    let discovery_response = query_proxied_server(&remote_address);
+    let discovery_response = query_proxied_server(&remote_address)
+        .await
+        .expect("No valid response from remote server!");
     let local_socket_address = config.local.as_socket_addr();
     let remote_socket_address = SocketAddr::new(remote_address, discovery_response.get_port());
-    let disocvery_service_handle = discovery::start(discovery_response, &local_socket_address);
-    proxy::run(&local_socket_address, &remote_socket_address);
-    disocvery_service_handle.join().unwrap();
+    discovery::start(discovery_response, &local_socket_address)
+        .await
+        .expect("Failed to setup discovery!");
+    proxy::run(&local_socket_address, &remote_socket_address)
+        .await
+        .expect("Failed to proxy!");
+    Ok(())
 }
